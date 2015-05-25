9b3087bfdb73bc16f2018281fe91d9d1a6b65fcc
mellanot <amitbe@mellanox.com>
2015-05-25 16:20:10 +0300

    change to sidemenu

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,25 +1,84 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+  // Form data for the login modal
+  $scope.loginData = {};
 
-.controller('ChatsCtrl', function($scope, Demo) {
-  $scope.chats = Demo.all();
-  $scope.remove = function(chat) {
-    Demo.remove(chat);
-  }
-})
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
 
-.controller('CompanyCtrl', function($scope, Demo) {
-  $scope.company = Demo.companies();
-  $scope.agents = Demo.agents();
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
   };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+})
+.controller('CompanyCtrl', function($scope, $stateParams) {
+  $scope.companies = [
+    {
+      id: 0,
+      name: 'Elbit Systems',
+      address: 'Hatavor 2 Yokneam, Israel',
+      location: "32.66624/35.10404",
+      face: 'http://upload.wikimedia.org/wikipedia/commons/2/24/New_Elbit_Logo.jpg'
+    }
+  ];
+  $scope.agents = [{
+      id: 0,
+      name: 'Ben Sparrow',
+      title: 'Regional Manager',
+      face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+    }, {
+      id: 1,
+      name: 'Max Lynx',
+      title: 'Assistant to the Regional Manager',
+      face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+    },{
+      id: 2,
+      name: 'Adam Bradleyson',
+      title: 'Sales Representative',
+      face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+    }, {
+      id: 3,
+      name: 'Perry Governor',
+      title: 'Regional Co-Manager',
+      face: 'https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg'
+    }, {
+      id: 4,
+      name: 'Mike Harrington',
+      title: 'Sales Representative',
+      face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
+    }];
+})
+.controller('PlaylistsCtrl', function($scope) {
+  $scope.playlists = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+})
+
+.controller('PlaylistCtrl', function($scope, $stateParams) {
 });
